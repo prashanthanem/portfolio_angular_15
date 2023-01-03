14b2030f8808e780119f259ead77cff3b92f7e5d
Prashanth Anem <prashanthanem@Prashanths-MacBook-Pro.local>
2023-01-02 23:08:27 -0500

    Image carousel component has been added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavmenuComponent } from './navmenu/navmenu.component';
+import { ImgCarouselComponent } from './img-carousel/img-carousel.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavmenuComponent
+    NavmenuComponent,
+    ImgCarouselComponent
   ],
   imports: [
     BrowserModule,
