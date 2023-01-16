71f899e7a361fd5d4266cb6466c9fa5a283717db
Prashanth Anem <prashanthanem@Prashanths-MacBook-Pro.local>
2023-01-16 16:41:10 -0500

    Add EmailJs support for contact form

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
-
+import { Component, ViewChild } from '@angular/core';
+import { Contact } from './contact';
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent {
+  @ViewChild('contactForm') form : any;
 
+  contactModel = new Contact('', '', '');
+
+  onClickSubmit(){
+    console.log('contactForm', this.contactModel);
+    this.form.reset();
+    
+    // this.contactModel.name = '';
+    // this.contactModel.message = '';
+    // this.contactModel.email = '';
+
+  }
 }
