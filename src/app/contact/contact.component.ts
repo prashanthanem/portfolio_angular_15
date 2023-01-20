ad855ad2d44b815c3b857152aa72b6b04449482b
Prashanth Anem <prashanthanem@Prashanths-MacBook-Pro.local>
2023-01-20 01:53:35 -0500

    Resume details updated contact form is in progress

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,5 +1,7 @@
 import { Component, ViewChild } from '@angular/core';
 import { Contact } from './contact';
+import emailjs, { EmailJSResponseStatus } from '@emailjs/browser';
+
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
@@ -9,14 +11,29 @@ export class ContactComponent {
   @ViewChild('contactForm') form : any;
 
   contactModel = new Contact('', '', '');
+  submitted = false;
+  loading = false;
 
   onClickSubmit(){
+    this.loading=true
     console.log('contactForm', this.contactModel);
-    this.form.reset();
-    
-    // this.contactModel.name = '';
-    // this.contactModel.message = '';
-    // this.contactModel.email = '';
+    this.sendEmail();
+  }
 
+  public sendEmail() {
+    const template_params = {
+      'from_name': this.contactModel.name,
+      'from_email': this.contactModel.email,
+      'message': this.contactModel.message
+  };
+    emailjs.send('service_w6c0nmt', 'template_qjdc26i', template_params, 'yffB3k72wE-CDiycd')
+      .then((result: EmailJSResponseStatus) => {
+        console.log(result.text);
+        this.submitted = true;
+        this.form.reset();
+        this.loading= false;
+      }, (error) => {
+        console.log(error.text);
+      });
   }
 }
