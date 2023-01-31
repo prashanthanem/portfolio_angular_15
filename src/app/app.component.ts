3f26136c432b15589651cb20718281c436947d7c
Prashanth Anem <prashanthanem@Prashanths-MacBook-Pro.local>
2023-01-31 13:30:37 -0500

    changed from SPA to single page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
 import { NavmenuComponent } from './navmenu/navmenu.component';
 import {RouterLinkWithHref, RouterOutlet} from "@angular/router";
+import { IntroArticleComponent } from './intro-article/intro-article.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ResumeComponent } from './resume/resume.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
-  imports: [NavmenuComponent, RouterLinkWithHref, RouterOutlet],
+  imports: [NavmenuComponent, RouterLinkWithHref, RouterOutlet, IntroArticleComponent, SkillsComponent, ResumeComponent, ContactComponent, FooterComponent],
   standalone : true,
   selector: 'app-root',
   templateUrl: './app.component.html',
