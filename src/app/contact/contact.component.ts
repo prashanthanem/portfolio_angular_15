6a67c42a283fa05f8be01ea461a9b11418f1c6e1
Prashanth Anem <prashanthanem@macbookpro.home>
2025-09-13 18:28:19 -0400

    Migrated to ANgular 19

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,12 +1,11 @@
 import { Component, ViewChild } from '@angular/core';
 import { Contact } from './contact';
 import emailjs, { EmailJSResponseStatus } from '@emailjs/browser';
-import { FooterComponent } from '../footer/footer.component';
+//import { FooterComponent } from '../footer/footer.component';
 import { FormsModule } from '@angular/forms';
 
 @Component({
-  imports : [FooterComponent, FormsModule],
-  standalone : true,
+  imports: [FormsModule],
   selector: 'contact',
   templateUrl: './contact.component.html'
 })
